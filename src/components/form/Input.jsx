0c798793314808e28107766ea94e9f9f31c75e8d
hassanullahusmani45 <hassanullahusmani45@gmail.com>
2024-12-30 23:30:58 +0430

    Feat: we well add the input component logic

--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -2,10 +2,32 @@ import React from 'react'
 
 export default function Input(props) {
 
+    const onChangeHandler = (event) => {
+        console.log("on chage");
+    }
+
+    const element = props.element === "input" ? (
+        <input
+            type={props.type}
+            id={props.id}
+            placeholder={props.placeholder}
+            className={`${props.className} ${props.isValid ? "border-green-500 focus:outline-green-500" : "border-red-500 focus:outline-red-500"}`}
+            onChange={onChangeHandler}
+            isValid={props.isValid}
+        />
+    ) : (
+        <textarea
+            id={props.id}
+            placeholder={props.placeholder}
+            className={`${props.className} ${props.isValid ? "border-green-500 focus:outline-green-500" : "border-red-500 focus:outline-red-500"}`}
+            onChange={props.onChangeHandler}
+            isValid={props.isValid}
+        />
+    );
 
     return (
         <div>
-            input
+            {element}
         </div>
     )
 }
