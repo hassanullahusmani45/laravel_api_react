8c39d242e7ff2f5ba10724cb5e7904788655cfef
hassanullahusmani45 <hassanullahusmani45@gmail.com>
2025-01-01 22:31:55 +0430

    Feat: The regex code for make the rules

---
+++ src/validators/Regex.jsx
@@ -0,0 +1,9 @@
+
+const Regex={
+    isValidEmail:(email)=>{
+        const emailPattern = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        return emailPattern.test(email);
+    }
+}
+
+export default Regex;
