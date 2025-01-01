4843c9035f7d57561c9b69ab11d29d2d9c6e862f
hassanullahusmani45 <hassanullahusmani45@gmail.com>
2025-01-01 22:23:09 +0430

    Feat: Codeing of the registration and the validation and deynamic error showing lougic

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -4,33 +4,58 @@ import { Link } from 'react-router-dom'
 import Logo from "../../assets/images/logo-white.svg";
 import { EnvelopeIcon, LockClosedIcon, UserIcon } from '@heroicons/react/24/outline';
 import Button from '../../components/form/Button';
+import { required, min, max, email } from '../../validators/Rules';
+import { useForm } from '../../hooks/useForm';
 
 export default function Register() {
 
-  const registerNewUser=(event)=>{
+  const [formState, onInputHandler] = useForm({
+    fullname: {
+      value: '',
+      isValid: false
+    },
+    email: {
+      value: '',
+      isValid: false
+    },
+    password: {
+      value: '',
+      isValid: false
+    }
+  }, false);
+
+  console.log(formState)
+  const registerNewUser = (event) => {
     event.preventDefault();
-    alert("refister")
+    alert("Register !!!")
   }
+
+
+
   return (
     <div className='flex justify-center w-full h-screen p-8 bg-slate-900 text-white'>
-
       <form className='flex flex-col justify-center  w-1/3 bg-slate-800 rounded-2xl shadow-2xl p-8'>
-
         <div className=' text-center text-green-500 text-lg font-medium'>Welcome To The Registration Page.</div>
 
-        <Link to={"/user/register"} className='flex justify-center items-center'>
+        <Link to={"/"} className='flex justify-center items-center'>
           <img src={Logo} className="w-24 h-24 rounded-full" alt="logo" />
         </Link>
 
-        <label htmlFor="Full-name"
+        <label htmlFor="fullname"
           className="block pl-2 mb-2 font-medium text-white dark:text-gray-200">Full-name :</label>
         <div className='relative'>
           <Input
-            id="Full-name"
+            id="fullname"
             element="input"
             type="text"
             placeholder="plece enter your full name."
             className="font-medium border-[3px] rounded-full shadow-md block w-full p-2 px-6 text-blue-800 bg-slate-200 placeholder:text-gray-500 placeholder:text-sm placeholder:font-normal"
+            validationes={[
+              required(),
+              min(3),
+              max(50),
+            ]}
+            onInputHandler={onInputHandler}
           />
           <UserIcon className='absolute right-3 top-3 size-5 text-slate-900' />
         </div>
@@ -43,8 +68,16 @@ export default function Register() {
             element="input"
             id="email"
             type="email"
-            placeholder="exmple@gmail.com"
+            placeholder="example@gmail.com"
             className="font-medium border-[3px] rounded-full shadow-md block w-full p-2 px-6 text-blue-800 bg-slate-200 placeholder:text-gray-500 placeholder:text-sm placeholder:font-normal"
+            validationes={[
+              required(),
+              email(),
+              min(9),
+              max(100),
+            ]}
+            onInputHandler={onInputHandler}
+
           />
           <EnvelopeIcon className='absolute right-3 top-3 size-5 text-slate-900' />
         </div>
@@ -59,6 +92,13 @@ export default function Register() {
             type="password"
             placeholder="********"
             className="font-medium border-[3px] rounded-full shadow-md block w-full p-2 px-6 text-blue-800 bg-slate-200 placeholder:text-gray-500 placeholder:text-sm placeholder:font-normal"
+            validationes={[
+              required(),
+              min(8),
+              max(20)
+            ]}
+            onInputHandler={onInputHandler}
+
           />
           <LockClosedIcon className='absolute right-3 top-3 size-5 text-slate-900' />
         </div>
@@ -68,13 +108,13 @@ export default function Register() {
             type="submit"
             onClick={registerNewUser}
             className="bg-green-500 py-1.5 px-6 w-fit rounded-full hover:scale-105"
+            disabled={!formState.isFormValid}
           >
             Register
           </Button>
         </div>
 
       </form>
-
     </div>
   )
 }
